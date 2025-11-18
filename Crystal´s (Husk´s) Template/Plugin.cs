faf5a80725b717d9405db4298617924706e97f00
husk <husk@DESKTOP-L67VITP>
2025-11-18 15:15:50 +0100

    Add project files.

---
+++ Crystal´s (Husk´s) Template/Plugin.cs
@@ -0,0 +1,80 @@
+﻿// THIS TEMPLATE WAS MADE BY CRYSTAL (HUSK)
+// TO USE THIS TEMPLATE WITHOUT ANY ISSUES PLEASE INSTALL GORILLA TAG OR HAVE GORILLA TAG ON YOUR C DRIVE WITH THE NEWEST UTILLA
+// AND BEPINEX
+using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+using Utilla.Attributes;
+
+namespace Crystal_s__Husk_s__Template
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    // Remove the // from modded game mod incase you want to use modded lobbies only on your mod if you want an non modded mod
+    // let the // be there and remove the following lines in the script: Line 61 ([ModdedGamemodeJoin]), Line 71 [ModdedGamemodeLeave]
+    //[ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void Start()
+        {
+            /* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            /* Undo mod setup here */
+            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
+            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+        }
+
+        void Update()
+        {
+            /* Code here runs every frame when the mod is enabled */
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            /* Activate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = true;
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            /* Deactivate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = false;
+        }
+    }
+}
---
+++ Crystal´s (Husk´s) Template/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Crystal_s__Husk_s__Template
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.changeme!.gorillatag.crystal´s (husk´s) template";
+        public const string Name = "Crystal_s__Husk_s__Template";
+        public const string Version = "1.0.0";
+    }
+}
